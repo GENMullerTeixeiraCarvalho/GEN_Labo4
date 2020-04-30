c5177cf4e9127a8bd1fe1e50a4a087006ac64e3d
Naludrag <stephanecarvalho81@gmail.com>
2020-04-30 15:42:43 +0200

    Refactor: Extraire une fonction, Classe: OrdersWriter, Méthode: getContent(), Mauvaise Odeur: Trop de ligne. Pas très lisible

--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -12,42 +12,7 @@ public class OrdersWriter {
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
             Order order = orders.getOrder(i);
-            sb.append("{");
-            sb.append("\"id\": ");
-            sb.append(order.getOrderId());
-            sb.append(", ");
-            sb.append("\"products\": [");
-            for (int j = 0; j < order.getProductsCount(); j++) {
-                Product product = order.getProduct(j);
-
-                sb.append("{");
-                sb.append("\"code\": \"");
-                sb.append(product.getCode());
-                sb.append("\", ");
-                sb.append("\"color\": \"");
-                sb.append(getColorFor(product));
-                sb.append("\", ");
-
-                if (product.getSize() != Product.SIZE_NOT_APPLICABLE) {
-                    sb.append("\"size\": \"");
-                    sb.append(getSizeFor(product));
-                    sb.append("\", ");
-                }
-
-                sb.append("\"price\": ");
-                sb.append(product.getPrice());
-                sb.append(", ");
-                sb.append("\"currency\": \"");
-                sb.append(product.getCurrency());
-                sb.append("\"}, ");
-            }
-
-            if (order.getProductsCount() > 0) {
-                sb.delete(sb.length() - 2, sb.length());
-            }
-
-            sb.append("]");
-            sb.append("}, ");
+            sb.append(formatOrder(order));
         }
 
         if (orders.getOrdersCount() > 0) {
@@ -55,6 +20,47 @@ public class OrdersWriter {
         }
 
         return sb.append("]}").toString();
+    }
+
+    private String formatOrder(Order order) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("{");
+        sb.append("\"id\": ");
+        sb.append(order.getOrderId());
+        sb.append(", ");
+        sb.append("\"products\": [");
+        for (int j = 0; j < order.getProductsCount(); j++) {
+            Product product = order.getProduct(j);
+
+            sb.append("{");
+            sb.append("\"code\": \"");
+            sb.append(product.getCode());
+            sb.append("\", ");
+            sb.append("\"color\": \"");
+            sb.append(getColorFor(product));
+            sb.append("\", ");
+
+            if (product.getSize() != Product.SIZE_NOT_APPLICABLE) {
+                sb.append("\"size\": \"");
+                sb.append(getSizeFor(product));
+                sb.append("\", ");
+            }
+
+            sb.append("\"price\": ");
+            sb.append(product.getPrice());
+            sb.append(", ");
+            sb.append("\"currency\": \"");
+            sb.append(product.getCurrency());
+            sb.append("\"}, ");
+        }
+
+        if (order.getProductsCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        sb.append("]");
+        sb.append("}, ");
+        return sb.toString();
     }
 
     private String getSizeFor(Product product) {
