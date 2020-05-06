01ee922f1d82d8b5ad23a3e408c8c65deef3c0f1
Naludrag <stephanecarvalho81@gmail.com>
2020-05-06 15:02:55 +0200

    Refactor: Déplacement de fonction, Classe: OrdersWriter, Méthode: getFormatOrder(), Mauvaise Odeur: Envie de fonctionnalité

--- src/main/java/ch/heigvd/gen2019/order/Order.java
+++ src/main/java/ch/heigvd/gen2019/order/Order.java
@@ -1,11 +1,12 @@
 package ch.heigvd.gen2019.order;
 
+import ch.heigvd.gen2019.Formattable;
 import ch.heigvd.gen2019.product.Product;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Order {
+public class Order implements Formattable {
     private List<Product> products = new ArrayList<Product>();
     private int id;
 
@@ -27,5 +28,24 @@ public class Order {
 
     public void AddProduct(Product product) {
         products.add(product);
+    }
+
+    public String getFormat(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("{");
+        sb.append(Formattable.formatField("id", this.getOrderId()));
+        sb.append(", ");
+        sb.append("\"products\": [");
+        for (int j = 0; j < this.getProductsCount(); j++) {
+            sb.append(this.getProduct(j).getFormat());
+        }
+
+        if (this.getProductsCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        sb.append("]");
+        sb.append("}, ");
+        return sb.toString();
     }
 }
--- src/main/java/ch/heigvd/gen2019/order/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/order/OrdersWriter.java
@@ -1,8 +1,5 @@
 package ch.heigvd.gen2019.order;
 
-import ch.heigvd.gen2019.Formattable;
-import ch.heigvd.gen2019.product.*;
-
 public class OrdersWriter {
     private Orders orders;
 
@@ -14,7 +11,7 @@ public class OrdersWriter {
         StringBuffer sb = new StringBuffer("{\"orders\": [");
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
-            sb.append(formatOrder(orders.getOrder(i)));
+            sb.append(orders.getOrder(i).getFormat());
         }
 
         if (orders.getOrdersCount() > 0) {
@@ -22,24 +19,5 @@ public class OrdersWriter {
         }
 
         return sb.append("]}").toString();
-    }
-
-    private String formatOrder(Order order) {
-        StringBuffer sb = new StringBuffer();
-        sb.append("{");
-        sb.append(Formattable.formatField("id", order.getOrderId()));
-        sb.append(", ");
-        sb.append("\"products\": [");
-        for (int j = 0; j < order.getProductsCount(); j++) {
-            sb.append(order.getProduct(j).getFormat());
-        }
-
-        if (order.getProductsCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-        }
-
-        sb.append("]");
-        sb.append("}, ");
-        return sb.toString();
     }
 }
