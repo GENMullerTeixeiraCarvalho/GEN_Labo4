a55ee62a54cda9af48ad7bce33820f2d32731ffc
Naludrag <stephanecarvalho81@gmail.com>
2020-05-06 15:06:39 +0200

    Refactor: Déplacement de fonction, Classe: OrdersWriter, Méthode: getContents(), Mauvaise Odeur: Envie de fonctionnalité

--- src/main/java/ch/heigvd/gen2019/order/Orders.java
+++ src/main/java/ch/heigvd/gen2019/order/Orders.java
@@ -1,9 +1,10 @@
 package ch.heigvd.gen2019.order;
 
 import java.util.ArrayList;
+import ch.heigvd.gen2019.Formattable;
 import java.util.List;
 
-public class Orders {
+public class Orders implements Formattable {
     private List<Order> orders = new ArrayList<Order>();
 
     public void AddOrder(Order order) {
@@ -16,5 +17,19 @@ public class Orders {
 
     public Order getOrder(int i) {
         return orders.get(i);
+    }
+
+    public String getFormat(){
+        StringBuffer sb = new StringBuffer("{\"orders\": [");
+
+        for (int i = 0; i < this.getOrdersCount(); i++) {
+            sb.append(this.getOrder(i).getFormat());
+        }
+
+        if (this.getOrdersCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        return sb.append("]}").toString();
     }
 }
